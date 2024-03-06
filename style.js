00a421f5928c6c6c097a61d5ecd9f19b77117b3a
Pavel Naruta <pasha.naruta@gmail.com>
2024-03-06 15:37:14 -0700

    up24

--- style.js
+++ style.js
@@ -2,27 +2,41 @@ document.addEventListener('DOMContentLoaded', function() {
     const dropdownTrigger = document.querySelector('.dropdown');
     const dropdownContent = document.querySelector('.dropdown-content');
     const arrow = document.querySelector('.dropdown .arrow');
+    const menuTrigger = document.querySelector('.menu-trigger');
+    const navbarContent = document.querySelector('.navbar-content');
 
-    let timeoutId;
+    if (dropdownTrigger && dropdownContent && arrow) {
+        // Function to show dropdown
+        function showDropdown() {
+            clearTimeout(timeoutId);
+            if (window.innerWidth > 550) {
+                dropdownContent.style.display = 'block';
+                arrow.style.transform = 'rotate(180deg)';
+            }
+        }
 
-    // Function to show dropdown
-    function showDropdown() {
-        clearTimeout(timeoutId);
-        dropdownContent.style.display = 'block';
-        dropdownContent.style.backgroundColor = '#ffffff';
-        arrow.style.transform = 'rotate(180deg)';
+        // Function to hide dropdown
+        let timeoutId;
+        function hideDropdown() {
+            timeoutId = setTimeout(() => {
+                dropdownContent.style.display = 'none';
+                arrow.style.transform = 'rotate(0deg)';
+            }, 300); // Delay for transition
+        }
+
+        dropdownTrigger.addEventListener('mouseenter', showDropdown);
+        dropdownTrigger.addEventListener('mouseleave', hideDropdown);
+        dropdownContent.addEventListener('mouseenter', showDropdown);
+        dropdownContent.addEventListener('mouseleave', hideDropdown);
     }
 
-    // Function to hide dropdown
-    function hideDropdown() {
-        timeoutId = setTimeout(() => {
-            dropdownContent.style.display = 'none';
-            arrow.style.transform = 'rotate(0deg)';
-        }, 300); // Delay to allow for transition to the margin area
-    }
+    if (menuTrigger && navbarContent) {
+        function toggleNavbar() {
+            if (window.innerWidth <= 550) {
+                navbarContent.classList.toggle('menu-active');
+            }
+        }
 
-    dropdownTrigger.addEventListener('mouseenter', showDropdown);
-    dropdownTrigger.addEventListener('mouseleave', hideDropdown);
-    dropdownContent.addEventListener('mouseenter', showDropdown);
-    dropdownContent.addEventListener('mouseleave', hideDropdown);
+        menuTrigger.addEventListener('click', toggleNavbar);
+    }
 });
